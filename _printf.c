aacbf9c4107666f102add64ce42a6d36e7733450
youness-mchaar <younessmchaar1999@gmail.com>
2023-04-24 07:51:44 -0700

    HELLO ALLS FILES

--- _printf.c
+++ _printf.c
@@ -1,67 +1,31 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
+/* BY EMOHAMEDD AND ABDELGHNI HAMANAR*/
 /**
- * _printf - "Congratulations,you win the jackpot!"
-
- * @format: format.
- * Return: Printed chars.
+ * _printf - our printf version for alx PLD
+ * @format: format
+ * Return: size_f
  */
+
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
+	int size_f = 0;
+	va_list args;
 
-if (format == NULL)
-return (-1);
+	if (format == NULL)
+	{
+		return (-1);
+	}
 
-va_start(list, format);
+	size_f = _strlen(format);
+	if (size_f <= 0)
+		return (0);
 
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-}
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-i++;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
-}
+	va_start(args, format);
+	size_f = checker(format, args);
 
-print_buffer(buffer, &buff_ind);
+	_putchar(-1);
+	va_end(args);
 
-va_end(list);
-
-return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-
-*buff_ind = 0;
+	return (size_f);
 }
