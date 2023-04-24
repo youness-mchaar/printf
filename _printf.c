896189c9d0db61650c893ac3299b4974beb9c58b
youness-mchaar <younessmchaar1999@gmail.com>
2023-04-24 08:08:08 -0700

    HELLO

--- _printf.c
+++ _printf.c
@@ -1,98 +1,66 @@
-include "main.h"
-/**
-*call_sp - fun that get the fun and call it and check
-*for signs
-*@format: the str format
-*@i: pointer to the index of our format
-*@p: pointer to out struct
-*@pCount: pointer to our counter
-*@pa:pointer to our arguments
-*/
-void call_sp(const char *format, int *i, struct sp_char *p,
-int *pCount, va_list pa)
-{
-	int j, k = 3;
-	va_list ap; /* to copy the pa */
-	int index = *i;
-	int flagg = 0;
-	sign flag[] = {{'+', postive_sign}, {' ', space_sign}, {'#', window_sign},
-		{'\0', NULL}};
+#include "main.h"
 
-	va_copy(ap, pa); /* copy the list of the arguments */
-	while (signIndex(format[index], flag) != -1)
-	{
-		for (k = 0; flag[k].ch != '\0'; k++)/* if there are flags */
-		{
-			if (format[index] == flag[k].ch)
-			{
-				if (format[index] == '+' && flagg < 2)
-					flagg = 1;
-				else if (format[index] == '#')
-					flagg = 2;
-				index++;
-				break;
-			}
-		}
-	}
-	j = spIndex(format[index], p);/* get the index of the sp */
-	if (j != -1) /* make sure it match */
-	{
-		if (flag[k].ch != '\0')
-			flag[k].fun(flagg, flag[k].ch, j, ap, pCount);/* print flag */
-		p[j].fun(pa, pCount); /*print the argument  */
-		*i = index;
-	}
-	else
-	{
-		_putchar('%');
-		(*i)--;
-		*pCount += 1;
-		return;
-	}
-}
-
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
-*_printf - fun that do same as printf
-*@format: the string format
-*Return: num of charchter printed
-*/
+ * _printf - "Congratulations,you win the jackpot!"
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-	va_list pa; /* points to the arguments list */
-	int i, count = 0;
-	int *pCount = &count;
-	spChar type[] = {
-		{'s', print_str}, {'c', print_ch}, {'d', print_int},
-		{'i', print_int}, {'b', print_bi}, {'r', print_rev},
-		{'u', print_unsigned}, {'o', print_octal}, {'S', print_nonch},
-		{'x', print_lowerhex}, {'X', print_upperhex}, {'R', print_rot13},
-		{'p', print_addr}, {'\0', NULL}};
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+if (format == NULL)
+return (-1);
 
-	va_start(pa, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]); /* print the char */
-			*pCount += 1;
-		}
-		else if (format[i] == '%' && format[i + 1] != '%')
-		{
-			i++;/* get the char after the % */
-			call_sp(format, &i, type, pCount, pa);
+va_start(list, format);
 
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			i++;
-			_putchar(format[i]);
-			*pCount += 1;
-		}
-	}
-	va_end(pa);
-	return (count);
+for (i = 0; format && format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+printed_chars++;
+}
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+i++;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
+
+print_buffer(buffer, &buff_ind);
+
+va_end(list);
+
+return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+
+*buff_ind = 0;
 }
