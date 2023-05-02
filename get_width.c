6124df4dc67695b775d3cc53c7dacf284594ad60
youness-mchaar <younessmchaar1999@gmail.com>
2023-05-02 16:16:57 -0700

    HELLO

--- get_width.c
+++ get_width.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -8,21 +9,21 @@
  *
  * Return: width.
  */
-int get_width(const char *format, int *j, va_list list)
+int get_width(const char *format, int *i, va_list list)
 {
-	int hello;
+	int curr_i;
 	int width = 0;
 
-	for (hello = *i + 1; format[hello] != '\0'; hello++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[hello]))
+		if (is_digit(format[curr_i]))
 		{
 			width *= 10;
-			width += format[hello] - '0';
+			width += format[curr_i] - '0';
 		}
-		else if (format[hello] == '*')
+		else if (format[curr_i] == '*')
 		{
-			hello++;
+			curr_i++;
 			width = va_arg(list, int);
 			break;
 		}
@@ -30,7 +31,8 @@ int get_width(const char *format, int *j, va_list list)
 			break;
 	}
 
-	*j = hello - 1;
+	*i = curr_i - 1;
 
 	return (width);
 }
+
